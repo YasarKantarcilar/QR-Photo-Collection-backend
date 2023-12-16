99fb1cdae43d9d66e76f198f4f8a0b7f9ba41920
YasarKantarcilar <kantarcilaryasar@gmail.com>
2023-12-16 20:21:00 +0300

    FEAT: add GOLANG FIBER to the project

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-	http.HandleFunc("/", serveMainPage)
-	http.HandleFunc("/loginPage", serveLoginPage)
-	http.HandleFunc("/login", handleLogin)
-	http.ListenAndServe(":8080", nil)
-}
+	app := fiber.New()
+	app.Use(logger.New())
 
-func serveMainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Serving request: %s\n, %s\n", r.Method, r.URL.Path)
-	http.ServeFile(w, r, "Pages/index.html")
-}
+	app.Get("/:test", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"status":  "success",
+			"message": c.Params("test"),
+		})
+	})
 
-func serveLoginPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Serving request: %s\n, %s\n", r.Method, r.URL.Path)
-	http.ServeFile(w, r, "Pages/login.html")
-}
-
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Serving request: %s\n, %s\n", r.Method, r.URL.Path)
-	myvar := map[string]interface{}{"email": r.FormValue("username"), "pass": r.FormValue("password")}
-	outputHTML(w, "Pages/index.html", myvar)
-}
-
-func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
-	t, err := template.ParseFiles(filename)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	if err := t.Execute(w, data); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
+	log.Fatal(app.Listen(":8000"))
 }
